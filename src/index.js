228fcb833f3ca48aa32b402a4dfab6273230c31f
Brian Scherner <brianscherner@gmail.com>
2024-04-22 15:30:11 -0700

    Add bootstrap to application.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
