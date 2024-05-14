332b3a15700dcbe9ebaa8e0257535066bfe00a79
Brian Scherner <brianscherner@gmail.com>
2024-05-14 14:55:20 -0700

    Refactor Miscellaneous form fields into their own component and import into ReusableForm.

---
+++ src/components/forms/MiscellaneousFields.js
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function MiscellaneousFields(props) {
+  const { trip, tripType } = props;
+
+  const communications = <input type="text" name="communications" placeholder="Cell and Wifi Service" defaultValue={props.trip !== undefined ? props.trip.communications : ''}/>
+
+  const gratuity = <input type="text" name="gratuity" placeholder="Gratuity Guidelines" defaultValue={props.trip !== undefined ? props.trip.gratuity : ''}/>
+
+  const guidedOrNot =
+  <select name="guidedOrNot">
+    <option value="" disabled selected>Guided?</option>
+    <option value="yes">Yes</option>
+    <option value="no">No</option>
+  </select>
+
+  const waterFees =
+  <select name="waterFees">
+    <option value="" disabled selected>Water Fees</option>
+    <option value="private">Private</option>
+    <option value="public">Public</option>
+  </select>
+
+  const access = <input type="text" name="access" placeholder="Access (ex: car, boat, etc)" defaultValue={props.trip !== undefined ? props.trip.access : ''}/>
+
+  const travelTime = <input type="text"
+  name="travelTime" placeholder="Travel Time" defaultValue={props.trip !== undefined ? props.trip.travelTime : ''}/>
+
+  const timeOfDay = <input type="text" name="timeOfDay" placeholder="Time of Day" defaultValue={props.trip !== undefined ? props.trip.timeOfDay : ''} required/>
+
+  const licenses = <input type="text" name="licenses" placeholder="Licenses" defaultValue={props.trip !== undefined ? props.trip.licenses : ''} required/>
+
+  return (
+    <React.Fragment>
+      <h4>Miscellaneous</h4>
+      {licenses}
+      <br/>
+      {waterFees}
+      <br/>
+      {access}
+      <br/>
+      {timeOfDay}
+      <br/>
+      {travelTime}
+      <br/>
+      {tripType === "Future" && (
+        <React.Fragment>
+          {communications}
+          <br/>
+          {gratuity}
+          <br/>
+          {guidedOrNot}
+          <br/>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  )
+}
+
+MiscellaneousFields.propTypes = {
+  trip: PropTypes.object,
+  tripType: PropTypes.string
+}
+
+export default MiscellaneousFields;
--- src/components/forms/ReusableTripForm.js
+++ src/components/forms/ReusableTripForm.js
@@ -1,17 +1,12 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import DestinationInfoFields from "./DestinationInfoFields";
-import GearRequirements from "./GearRequirementsFields";
+import GearRequirementsFields from "./GearRequirementsFields";
+import MiscellaneousFields from "./MiscellaneousFields";
 
 function ReusableTripForm(props) {
   const { trip } = props;
   const [tripType, setTripType] = useState('');
-
-  const licenses = <input type="text" name="licenses" placeholder="Licenses" defaultValue={props.trip !== undefined ? props.trip.licenses : ''} required/>
-
-  // past trip props //
-
-  const timeOfDay = <input type="text" name="timeOfDay" placeholder="Time of Day" defaultValue={props.trip !== undefined ? props.trip.timeOfDay : ''} required/>
 
   const fliesUsed = <input type="text" name="fliesUsed" placeholder="Flies Used" defaultValue={props.trip !== undefined ? props.trip.fliesUsed : ''} required/>
 
@@ -21,30 +16,9 @@ function ReusableTripForm(props) {
 
   const riverFlowLevels = <input type="text" name="riverFlowLevels" placeholder="River Flow Levels (cfs/ft)" defaultValue={props.trip !== undefined ? props.trip.riverFlowLevels : ''}/>
 
-  const travelTime = <input type="text"
-  name="travelTime" placeholder="Travel Time" defaultValue={props.trip !== undefined ? props.trip.travelTime : ''}/>
-
-  const access = <input type="text" name="access" placeholder="Access (ex: car, boat, etc)" defaultValue={props.trip !== undefined ? props.trip.access : ''}/>
-
   const fishingMethod = <input type="text" name="fishingMethod" placeholder="Fishing Method (wade, raft, etc)" defaultValue={props.trip !== undefined ? props.trip.fishingMethod : ''}/>
 
-  const waterFees =
-  <select name="waterFees">
-    <option value="" disabled selected>Water Fees</option>
-    <option value="private">Private</option>
-    <option value="public">Public</option>
-  </select>
-
-  // future trip props //
-
   const climate = <input type="text" name="climate" placeholder="Climate" defaultValue={props.trip !== undefined ? props.trip.climate : ''} required/>
-
-  const guidedOrNot =
-  <select name="guidedOrNot">
-    <option value="" disabled selected>Guided?</option>
-    <option value="yes">Yes</option>
-    <option value="no">No</option>
-  </select>
 
   const travelCosts = <input type="text" name="travelCosts" placeholder="Travel Costs" defaultValue={props.trip !== undefined ? props.trip.travelCosts : ''}/>
 
@@ -61,8 +35,6 @@ function ReusableTripForm(props) {
 
   const cancellationPolicy = <input type="text" name="cancellationPolicy" placeholder="Cancellation Policy" defaultValue={props.trip !== undefined ? props.trip.cancellationPolicy : ''}/>
 
-
-
   const baggageLuggage = <input type="text"
   name="baggageLuggage" placeholder="Baggage/Luggage Policy" defaultValue={props.trip !== undefined ? props.trip.baggageLuggage : ''}/>
 
@@ -70,10 +42,6 @@ function ReusableTripForm(props) {
   name="tripInsurance" placeholder="Trip Insurance" defaultValue={props.trip !== undefined ? props.trip.tripInsurance : ''}/>
 
   const evacInsurance = <input type="text" name="evacInsurance" placeholder="Medical Evacuation Insurance" defaultValue={props.trip !== undefined ? props.trip.evacInsurance : ''}/>
-
-  const communications = <input type="text" name="communications" placeholder="Cell and Wifi Service" defaultValue={props.trip !== undefined ? props.trip.communications : ''}/>
-
-  const gratuity = <input type="text" name="gratuity" placeholder="Gratuity Guidelines" defaultValue={props.trip !== undefined ? props.trip.gratuity : ''}/>
 
   // button which renders when a trip type is selected //
 
@@ -89,11 +57,17 @@ function ReusableTripForm(props) {
         </select>
         <br/>
         {tripType && (
-          <DestinationInfoFields trip={trip} tripType={tripType}/>
+          <React.Fragment>
+            <DestinationInfoFields trip={trip} tripType={tripType}/>
+          </React.Fragment>
         )}
 
         {tripType === "Future" && (
-          <GearRequirements trip={trip} tripType={tripType}/>
+          <GearRequirementsFields trip={trip} tripType={tripType}/>
+        )}
+
+        {tripType && (
+          <MiscellaneousFields trip={trip} tripType={tripType}/>
         )}
       </form>
       <br/>
