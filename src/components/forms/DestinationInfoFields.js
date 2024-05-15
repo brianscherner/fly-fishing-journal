2981259f7b279fa81c0ce7ac3df3a07fae42d5fa
Brian Scherner <brianscherner@gmail.com>
2024-05-15 09:37:29 -0700

    Add defaultValue attributes to dropdown menu fields.

--- src/components/forms/DestinationInfoFields.js
+++ src/components/forms/DestinationInfoFields.js
@@ -7,21 +7,21 @@ function DestinationInfoFields(props) {
   const destination = <input type="text" name="destination" placeholder="Destination" defaultValue={trip !== undefined ? trip.destination : ''} required/>
 
   const destinationType =
-  <select name="destinationType">
+  <select name="destinationType" defaultValue={trip !== undefined ? trip.destinationType : ''}>
     <option value="" disabled selected>Destination Type</option>
     <option value="domestic">Domestic</option>
     <option value="international">International</option>
   </select>
 
   const waterType =
-  <select name="waterType">
+  <select name="waterType" defaultValue={trip !== undefined ? trip.waterType : ''}>
     <option value="" disabled selected>Water Type</option>
     <option value="freshwater">Freshwater</option>
     <option value="saltwater">Saltwater</option>
   </select>
 
   const waterBodyType =
-  <select name="waterBodyType">
+  <select name="waterBodyType" defaultValue={trip !== undefined ? trip.waterBodyType : ''}>
     <option value="" disabled selected>Water Body Type</option>
     <option value="river">River</option>
     <option value="lake">Lake</option>
@@ -32,7 +32,7 @@ function DestinationInfoFields(props) {
   const species = <input type="text" name="species" placeholder="Fish Species" defaultValue={trip !== undefined ? trip.species : ''} required/>
 
   const season =
-  <select name="season">
+  <select name="season" defaultValue={trip !== undefined ? trip.season : ''}>
     <option value="" disabled selected>Season</option>
     <option value="winter">Winter</option>
     <option value="spring">Spring</option>
@@ -46,7 +46,7 @@ function DestinationInfoFields(props) {
 
   const country = <input type="text" name="country" placeholder="Country" defaultValue={trip !== undefined ? trip.country : ''} required/>
 
-  const climate = <input type="text" name="climate" placeholder="Climate" defaultValue={props.trip !== undefined ? props.trip.climate : ''} required/>
+  const climate = <input type="text" name="climate" placeholder="Climate" defaultValue={trip !== undefined ? trip.climate : ''} required/>
 
   return (
     <React.Fragment>
