6e56265ab26334dbf1b0a87dc8db61da3b3a41ca
Brian Scherner <brianscherner@gmail.com>
2024-05-24 16:03:07 -0700

    Add accordion menu to display trips by their type.

--- src/components/trips/TripsList.js
+++ src/components/trips/TripsList.js
@@ -22,19 +22,33 @@ function TripsList(props) {
 
   return (
     <React.Fragment>
-      <button type="button" onClick={allTrips} className="btn btn-primary app-buttons">All Trips</button>
-      <button type="button" onClick={filterByPast} className="btn btn-primary app-buttons">Past Trips</button>
-      <button type="button" onClick={filterByFuture} className="btn btn-primary app-buttons">Future Trips</button>
-      <br/>
-      <br/>
-      {filteredTripsList.map((trip) =>
-        <Trip
-          whenTripClicked={props.onTripSelection}
-          destination={trip.destination}
-          tripType={trip.tripType}
-          id={trip.id}
-          key={trip.id}/>
-      )}
+      <div className="accordion accordion-flush">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed" type="button" onClick={allTrips}>All Trips</button>
+          </h2>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed" type="button" onClick={filterByPast}>Past Trips</button>
+          </h2>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button className="accordion-button collapsed" type="button" onClick={filterByFuture} >Future Trips</button>
+          </h2>
+        </div>
+        <br/>
+        <br/>
+        {filteredTripsList.map((trip) =>
+          <Trip
+            whenTripClicked={props.onTripSelection}
+            destination={trip.destination}
+            tripType={trip.tripType}
+            id={trip.id}
+            key={trip.id}/>
+        )}
+        </div>
+      </div>
     </React.Fragment>
   );
 }
