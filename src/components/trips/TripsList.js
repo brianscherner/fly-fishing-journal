4022a5c2f29e512a03f064ed14a9a029e5c07005
Brian Scherner <brianscherner@gmail.com>
2024-05-24 14:58:24 -0700

    Add functionality to filter trips list by trip type (work-in-progress).

--- src/components/trips/TripsList.js
+++ src/components/trips/TripsList.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Trip from './Trip';
 import PropTypes from 'prop-types';
 
+// I want to show a list of all trips and have a button that will toggle trips by their type (by 'past' or 'future') //
+
 function TripsList(props) {
+  const [tripsList, setTripsList] = useState(props.tripsList);
+  const [filteredTripsList, setFilteredTripsList] = useState(props.tripsList);
+
+  const filterByPast = () => {
+    const filteredPastTripsList = tripsList.filter(trip => trip.tripType === "Past");
+    setFilteredTripsList(filteredPastTripsList);
+    console.log(filteredPastTripsList);
+  }
+
+  const filterByFuture = () => {
+    const filteredFutureTripsList = tripsList.filter(trip => trip.tripType === "Future");
+    setFilteredTripsList(filteredFutureTripsList);
+    console.log(filteredFutureTripsList);
+  }
+
+  const allTrips = () => {
+    setTripsList(tripsList);
+    console.log(tripsList);
+  }
+
   return (
     <React.Fragment>
-      {props.tripsList.map((trip) =>
+      <button type="button" onClick={allTrips} className="btn btn-primary app-buttons">All Trips</button>
+      <button type="button" onClick={filterByPast} className="btn btn-primary app-buttons">Past Trips</button>
+      <button type="button" onClick={filterByFuture} className="btn btn-primary app-buttons">Future Trips</button>
+      <br/>
+      <br/>
+      {filteredTripsList.map((trip) =>
         <Trip
           whenTripClicked={props.onTripSelection}
           destination={trip.destination}
@@ -18,8 +45,10 @@ function TripsList(props) {
 }
 
 TripsList.propTypes = {
+  buttonText: PropTypes.string,
   tripsList: PropTypes.array,
-  onTripSelection: PropTypes.func
+  onTripSelection: PropTypes.func,
+  filterByTripType: PropTypes.func,
 }
 
 export default TripsList;
