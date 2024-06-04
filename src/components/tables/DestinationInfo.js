8f646030fed695937e157009dae8ea85c491cdb1
Brian Scherner <brianscherner@gmail.com>
2024-06-04 15:12:48 -0700

    Display 'N/A' in an empty table row. for DestinationInfo.

--- src/components/tables/DestinationInfo.js
+++ src/components/tables/DestinationInfo.js
@@ -12,60 +12,60 @@ function DestinationInfo(props) {
             <tbody>
               <tr className="dest-info-details">
                 <th>Destination</th>
-                <td className="dest-info-data">{trip.destination}</td>
+                <td className="dest-info-data">{trip.destination ? trip.destination : "N/A"}</td>
               </tr>
               <tr className="dest-info-details">
                 <th>Trip Type</th>
-                <td className="dest-info-data">{trip.tripType}</td>
+                <td className="dest-info-data">{trip.tripType ? trip.tripType : "N/A"}</td>
               </tr>
               <tr className="dest-info-details">
                 <th>Season</th>
-                <td className="dest-info-data">{trip.season}</td>
+                <td className="dest-info-data">{trip.season ? trip.season : "N/A"}</td>
               </tr>
               <tr className="dest-info-details">
                 <th>Start Date</th>
-                <td className="dest-info-data">{trip.startDate}</td>
+                <td className="dest-info-data">{trip.startDate ? trip.startDate : "N/A"}</td>
               </tr>
               <tr className="dest-info-details">
                 <th>End Date</th>
-                <td className="dest-info-data">{trip.endDate}</td>
+                <td className="dest-info-data">{trip.endDate ? trip.endDate : "N/A"}</td>
               </tr>
               <tr className="dest-info-details">
                 <th>Water Type</th>
-                <td className="dest-info-data">{trip.waterType}</td>
+                <td className="dest-info-data">{trip.waterType ? trip.waterType : "N/A"}</td>
               </tr>
               <tr className="dest-info-details">
                 <th>Water Body Type</th>
-                <td className="dest-info-data">{trip.waterBodyType}</td>
+                <td className="dest-info-data">{trip.waterBodyType ? trip.waterBodyType : "N/A"}</td>
               </tr>
               <tr className="dest-info-details">
                 <th>Fish Species</th>
-                <td className="dest-info-data">{trip.species}</td>
+                <td className="dest-info-data">{trip.species ? trip.species : "N/A"}</td>
               </tr>
 
               {trip.tripType === "Past" && (
                 <React.Fragment>
                   <tr className="dest-info-details">
                     <th>State</th>
-                    <td className="dest-info-data">{trip.state}</td>
+                    <td className="dest-info-data">{trip.state ? trip.state : "N/A"}</td>
                   </tr>
                   <tr className="dest-info-details">
                     <th>County</th>
-                    <td className="dest-info-data">{trip.county}</td>
+                    <td className="dest-info-data">{trip.county ? trip.county : "N/A"}</td>
                   </tr>
                 </React.Fragment>
               )}
 
               <tr className="dest-info-details">
                 <th>Country</th>
-                <td className="dest-info-data">{trip.country}</td>
+                <td className="dest-info-data">{trip.country ? trip.country : "N/A"}</td>
               </tr>
 
               {trip.tripType === "Future" && (
                 <React.Fragment>
                   <tr className="dest-info-details">
                     <th>Climate</th>
-                    <td className="dest-info-data">{trip.climate}</td>
+                    <td className="dest-info-data">{trip.climate ? trip.climate : "N/A"}</td>
                   </tr>
                 </React.Fragment>
               )}
