9ba6e1a7684560af247d7d04823b68692bcfe23d
Brian Scherner <brianscherner@gmail.com>
2024-06-24 10:28:27 -0700

    Add toast notifications for user sign up.

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "../firebase.js";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function SignUp() {
-  const [messageToUser, setMessageToUser] = useState(null);
+  const [userSignedUp, setUserSignedUp] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((createdUser) => {
+      if (createdUser) {
+        setUserSignedUp(true);
+      } else {
+        setUserSignedUp(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   function doSignUp(event) {
     event.preventDefault();
@@ -11,38 +25,46 @@ function SignUp() {
     const password = event.target.password.value;
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        setMessageToUser(`Sign up successful. Welcome ${userCredential.user.email}!`);
+        toast.success(`Sign up successful. Welcome, ${userCredential.user.email}!`, {
+          position: "bottom-right"});
       })
       .catch((error) => {
-        setMessageToUser(`There was an error signing up: ${error.message}`);
+        toast.error(`There was an error signing up: ${error.message}`, {
+          position: "bottom-right"});
       });
   }
 
   return (
     <React.Fragment>
-      {messageToUser && (
-        <p className="sign-in-messages" style={{color: messageToUser.toLowerCase().includes('error') ? '#dc3545' : '#198754'}}>{messageToUser}</p>
+      {!userSignedUp && (
+        <React.Fragment>
+          <h2 className="sign-in-headings">Sign Up</h2>
+          <div className="row justify-content-center">
+            <div className="col-6">
+              <form onSubmit={doSignUp}>
+                <input className="form-control"
+                  type="text"
+                  name="email"
+                  placeholder="Email"/>
+                <br/>
+                <input className="form-control"
+                  type="password"
+                  name="password"
+                  placeholder="Password"/>
+                <br/>
+                <button className="btn app-buttons" type="submit">Sign Up</button>
+              </form>
+              <br/>
+            </div>
+          </div>
+        </React.Fragment>
       )}
 
-      <h2 className="sign-in-headings">Sign Up</h2>
-      <div className="row justify-content-center">
-        <div className="col-6">
-          <form onSubmit={doSignUp}>
-            <input className="form-control"
-              type="text"
-              name="email"
-              placeholder="Email"/>
-            <br/>
-            <input className="form-control"
-              type="password"
-              name="password"
-              placeholder="Password"/>
-            <br/>
-            <button className="btn app-buttons" type="submit">Sign Up</button>
-          </form>
-          <br/>
-        </div>
-      </div>
+      {userSignedUp && (
+        <h2 className='auth-message'>You must first sign out of your account before creating a new one.</h2>
+      )}
+
+      <ToastContainer/>
     </React.Fragment>
   )
 }
