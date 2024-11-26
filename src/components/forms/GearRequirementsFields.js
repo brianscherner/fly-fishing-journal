6a2a171ac3cf1dd2275a73922c444f8bb14d1372
Brian Scherner <brianscherner@gmail.com>
2024-11-25 19:20:33 -0800

    Refactor gear requirements form section.

--- src/components/forms/GearRequirementsFields.js
+++ src/components/forms/GearRequirementsFields.js
@@ -1,24 +1,48 @@
 import React from "react";
 import PropTypes from "prop-types";
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import PhishingIcon from '@mui/icons-material/Phishing';
+import BugReportIcon from '@mui/icons-material/BugReport';
 
 function GearRequirements(props) {
   const { formData, setFormData } = props;
 
-  const clothingRequirements = <input type="text" className="form-control" name="clothingRequirements" placeholder="Clothing Requirements*" defaultValue={formData.clothingRequirements ??= ''} onChange={(e) => setFormData({...formData, clothingRequirements: e.target.value})}/>
-
-  const gearRequirements = <input type="text" className="form-control" name="gearRequirements" placeholder="Fishing Gear Requirements*" defaultValue={formData.gearRequirements ??= ''} onChange={(e) => setFormData({...formData, gearRequirements: e.target.value})}/>
-
-  const flyRequirements = <input type="text" className="form-control" name="flyRequirements" placeholder="Fly Requirements*" defaultValue={formData.flyRequirements ??= ''} onChange={(e) => setFormData({...formData, flyRequirements: e.target.value})}/>
-
   return (
     <React.Fragment>
       <h4 className="form-section-heading">Gear Requirements</h4>
-      <p className="required-msg">* indicates a required field</p>
-      {clothingRequirements}
+      <p className="required-msg" style={{ fontStyle: "italic"}}>* indicates a required field</p>
+      <label>Clothing Requirements*</label>
+      <div className="form-input-container">
+        <input
+          type="text"
+          className="form-control"
+          name="clothingRequirements"
+          placeholder="Enter requirements"
+          defaultValue={formData.clothingRequirements ??= ''} onChange={(e) => setFormData({...formData, clothingRequirements: e.target.value})}/>
+        <CheckroomIcon className="form-input-icon"/>
+      </div>
       <br/>
-      {gearRequirements}
+      <label>Fishing Requirements*</label>
+      <div className="form-input-container">
+        <input
+          type="text"
+          className="form-control"
+          name="gearRequirements"
+          placeholder="Enter requirements"
+          defaultValue={formData.gearRequirements ??= ''} onChange={(e) => setFormData({...formData, gearRequirements: e.target.value})}/>
+        <PhishingIcon className="form-input-icon"/>
+      </div>
       <br/>
-      {flyRequirements}
+      <label>Fly Requirements*</label>
+      <div className="form-input-container">
+        <input
+          type="text"
+          className="form-control"
+          name="flyRequirements"
+          placeholder="Enter requirements"
+          defaultValue={formData.flyRequirements ??= ''} onChange={(e) => setFormData({...formData, flyRequirements: e.target.value})}/>
+        <BugReportIcon className="form-input-icon"/>
+      </div>
       <br/>
     </React.Fragment>
   )
