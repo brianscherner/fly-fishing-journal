1bf45732d0aee6fa15ee04b9a5499a80dc49e8e1
Brian Scherner <brianscherner@gmail.com>
2024-11-25 19:11:38 -0800

    Refactor TripCosts form section.

--- src/components/forms/TripCostsFields.js
+++ src/components/forms/TripCostsFields.js
@@ -1,49 +1,106 @@
 import React from "react";
 import PropTypes from "prop-types";
+import FlightIcon from '@mui/icons-material/Flight';
+import CottageIcon from '@mui/icons-material/Cottage';
+import PaymentIcon from '@mui/icons-material/Payment';
+import CardTravelIcon from '@mui/icons-material/CardTravel';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import LuggageIcon from '@mui/icons-material/Luggage';
 
 function TripCostsFields(props) {
   const { formData, setFormData } = props;
 
-  const travelCosts = <input type="text" className="form-control" name="travelCosts" placeholder="Air Travel Costs" defaultValue={formData.travelCosts ??= ''} onChange={(e) => setFormData({...formData, travelCosts: e.target.value})}/>
-
-  const travelDocs =
-  <select name="travelDocs" className="form-select" defaultValue={formData.travelDocs ??= ''} onChange={(e) => setFormData({...formData, travelDocs: e.target.value})}>
-    <option value="" disabled>Travel Documents*</option>
-    <option value="Passport">Passport</option>
-    <option value="Visa">Visa</option>
-  </select>
-
-  const tripExpenses = <input type="text" className="form-control" name="tripExpenses" placeholder="Fishing & Lodging Expenses*" defaultValue={formData.tripExpenses ??= ''} onChange={(e) => setFormData({...formData, tripExpenses: e.target.value})}/>
-
-  const depositTerms = <input type="text" className="form-control" name="depositTerms" placeholder="Deposit Payment Terms*" defaultValue={formData.depositTerms ??= ''} onChange={(e) => setFormData({...formData, depositTerms: e.target.value})}/>
-
-  const cancellationPolicy = <input type="text" className="form-control" name="cancellationPolicy" placeholder="Refund Cancellation Policy*" defaultValue={formData.cancellationPolicy ??= ''} onChange={(e) => setFormData({...formData, cancellationPolicy: e.target.value})}/>
-
-  const baggageLuggage = <input type="text" className="form-control" name="baggageLuggage" placeholder="Baggage and Luggage Policy" defaultValue={formData.baggageLuggage ??= ''} onChange={(e) => setFormData({...formData, baggageLuggage: e.target.value})}/>
-
-  const tripInsurance = <input type="text" className="form-control" name="tripInsurance" placeholder="Trip Insurance*" defaultValue={formData.tripInsurance ??= ''} onChange={(e) => setFormData({...formData, tripInsurance: e.target.value})}/>
-
-  const evacInsurance = <input type="text" className="form-control" name="evacInsurance" placeholder="Medical Evacuation Insurance*" defaultValue={formData.evacInsurance ??= ''} onChange={(e) => setFormData({...formData, evacInsurance: e.target.value})}/>
-
   return (
     <React.Fragment>
       <h4 className="form-section-heading">Trip Costs</h4>
-      <p className="required-msg">* indicates a required field</p>
-      {travelDocs}
+      <p className="required-msg" style={{ fontStyle: "italic"}}>* indicates a required field</p>
+      <label>Travel Documents*</label>
+      <select
+        name="travelDocs"
+        className="form-select"
+        defaultValue={formData.travelDocs ??= ''}
+        onChange={(e) => setFormData({...formData, travelDocs: e.target.value})}>
+        <option value="" disabled>Select an option</option>
+        <option value="Passport">Passport</option>
+        <option value="Visa">Visa</option>
+      </select>
       <br/>
-      {travelCosts}
+      <label>Air Travel Cost</label>
+      <div className="form-input-container">
+        <input
+          type="text"
+          className="form-control"
+          name="travelCosts"
+          placeholder="Enter cost"
+          defaultValue={formData.travelCosts ??= ''} onChange={(e) => setFormData({...formData, travelCosts: e.target.value})}/>
+        <FlightIcon className="form-input-icon"/>
+      </div>
       <br/>
-      {tripExpenses}
+      <label>Fishing and Lodging Expenses*</label>
+      <div className="form-input-container">
+        <input
+          type="text"
+          className="form-control"
+          name="tripExpenses"
+          placeholder="Enter expenses" defaultValue={formData.tripExpenses ??= ''} onChange={(e) => setFormData({...formData, tripExpenses: e.target.value})}/>
+        <CottageIcon className="form-input-icon"/>
+      </div>
       <br/>
-      {depositTerms}
+      <label>Deposit Payment Terms*</label>
+      <div className="form-input-container">
+        <input
+          type="text"
+          className="form-control"
+          name="depositTerms"
+          placeholder="Enter terms"
+          defaultValue={formData.depositTerms ??= ''} onChange={(e) => setFormData({...formData, depositTerms: e.target.value})}/>
+        <PaymentIcon className="form-input-icon"/>
+      </div>
       <br/>
-      {tripInsurance}
+      <label>Trip Insurance*</label>
+      <div className="form-input-container">
+        <input
+          type="text"
+          className="form-control"
+          name="tripInsurance"
+          placeholder="Enter policy"
+          defaultValue={formData.tripInsurance ??= ''}
+          onChange={(e) => setFormData({...formData, tripInsurance: e.target.value})}/>
+        <CardTravelIcon className="form-input-icon"/>
+      </div>
       <br/>
-      {evacInsurance}
+      <label>Medical Evacuation Insurance*</label>
+      <div className="form-input-container">
+        <input
+          type="text"
+          className="form-control"
+          name="evacInsurance"
+          placeholder="Enter policy" defaultValue={formData.evacInsurance ??= ''} onChange={(e) => setFormData({...formData, evacInsurance: e.target.value})}/>
+        <HealthAndSafetyIcon className="form-input-icon"/>
+      </div>
       <br/>
-      {cancellationPolicy}
+      <label>Refund Cancellation Policy*</label>
+      <div className="form-input-container">
+        <input
+          type="text"
+          className="form-control"
+          name="cancellationPolicy"
+          placeholder="Enter policy"
+          defaultValue={formData.cancellationPolicy ??= ''} onChange={(e) => setFormData({...formData, cancellationPolicy: e.target.value})}/>
+        <ReceiptIcon className="form-input-icon"/>
+      </div>
       <br/>
-      {baggageLuggage}
+      <label>Baggage and Luggage Policy*</label>
+      <div className="form-input-container">
+        <input
+          type="text"
+          className="form-control"
+          name="baggageLuggage"
+          placeholder="Enter policy"
+          defaultValue={formData.baggageLuggage ??= ''} onChange={(e) => setFormData({...formData, baggageLuggage: e.target.value})}/>
+        <LuggageIcon className="form-input-icon"/>
+      </div>
       <br/>
     </React.Fragment>
   )
