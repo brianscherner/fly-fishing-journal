fb49988c1053f101d5732ea28d217818378d80fe
Brian Scherner <brianscherner@gmail.com>
2024-12-05 15:47:29 -0800

    WIP commit for displaying photos in grid format.

--- src/components/forms/Images.js
+++ src/components/forms/Images.js
@@ -2,8 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // Firebase doesn't support custom file objects, so it currently can't upload the images
-// also need a way to preview the images
-// need a way to limit more than 6 images being uploaded
 // need a way to delete images
 
 function Images(props) {
@@ -11,33 +9,34 @@ function Images(props) {
 
   return (
     <React.Fragment>
-      <h4 className="form-section-heading">Add Photos</h4>
-      <p className="required-msg">Upload up to 6 photos from your trip!</p>
-      {Array.from({ length: 6 }).map((_, index) => (
-        <div key={index} className="photo-upload-slot">
-          {!formData.images[index] ? (
-            <input
-              type="file"
-              accept="image/*"
-              // defaultValue={formData.images ??= ''}
-              onChange={(e) => onChangingFile(e, index)}
-            />
-          ) : (
-            <div className="photo-preview">
-              <img
-                src={formData.images[index].preview}
-                alt={`Preview ${index}`}
-                style={{
-                  width: "100px",
-                  height: "100px",
-                  objectFit: "cover"
-                }}
+      <h4 className="form-section-heading">Photos</h4>
+      <p className="required-msg">Add up to 6 photos from your trip!</p>
+      <div className="photo-upload-container">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <div key={index} className="photo-upload-slot">
+            {!formData.images[index] ? (
+              <input
+                type="file"
+                accept="image/*"
+                onChange={(e) => onChangingFile(e, index)}
               />
-              {/* <button onClick={() => handleDelete(index)}>Delete</button> */}
-            </div>
-          )}
-        </div>
-      ))}
+            ) : (
+              <div className="photo-preview">
+                <img
+                  src={formData.images[index].preview}
+                  alt={`Preview ${index}`}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    objectFit: "cover"
+                  }}
+                />
+                {/* <button onClick={() => handleDelete(index)}>Delete</button> */}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
       <br/>
     </React.Fragment>
   )
