26125e3b2eba2f7e10adc27021ff4c956bd94bcd
Brian Scherner <brianscherner@gmail.com>
2024-12-07 23:36:01 -0800

    Update styling of photo uploader button.

--- src/components/forms/Images.js
+++ src/components/forms/Images.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import DeleteIcon from '@mui/icons-material/Delete';
+import UploadIcon from '@mui/icons-material/Upload';
 
 function Images(props) {
   const { formData, onChangingImage, onDeletingImage } = props;
@@ -9,34 +10,29 @@ function Images(props) {
     <React.Fragment>
       <h4 className="form-section-heading">Photos</h4>
       <p className="required-msg">Add up to 6 photos from your trip!</p>
-      {/* need to give a more modern appearance - looks ugly */}
       <div className="photo-upload-slot">
-        <input
-          type="file"
-          accept="image/*"
-          multiple onChange={(e) => onChangingImage(e)}
-        />
+        <label className="upload-button">
+          <UploadIcon fontSize="medium" className="upload-icon"/>
+          <span className="upload-text">Upload</span>
+          <input
+            type="file"
+            accept="image/*"
+            multiple onChange={(e) => onChangingImage(e)}
+          />
+        </label>
       </div>
       <br/>
       <div className="photo-upload-container">
         {formData.images.map((image, index) => (
-          // need to style so that they're centered and can be responsive to other screen sizes
-          // hard coding with px values makes them overflow
           <div key ={index} className="photo-preview">
             <div className="photo-preview-container">
               <img
                 src={image.preview}
                 alt={`Preview ${index}`}
-                style={{
-                  width: "150px",
-                  height: "150px",
-                  objectFit: "cover",
-                  borderRadius: "0.45rem"
-                }}
+                className="photo"
               />
               <DeleteIcon
                 className="photo-delete-icon"
-                fontSize="large"
                 onClick={() => onDeletingImage(index)}/>
             </div>
           </div>
