775bfea952f8d73223bb3a4ac77284dd71a227a8
Brian Scherner <brianscherner@gmail.com>
2025-03-20 17:53:23 -0700

    Add filtering feature for water body type.

--- src/components/trips/TripsList.js
+++ src/components/trips/TripsList.js
@@ -3,42 +3,109 @@ import Trip from './Trip';
 
 function TripsList(props) {
   const { tripsList } = props;
-  const [filter, setFilter] = useState('');
+  const [filters, setFilters] = useState({
+    location: "",
+    waterBodyType: "",
+    season: "",
+    fishCaught: "",
+    tripType: ""
+  });
 
   const filteredTripsList = useMemo(() => {
-
-    if (filter === "Past") {
+    // filter by trip type
+    // filter by past trip type and additional criteria
+    if (filters.tripType === "Past") {
+      if (filters.waterBodyType === "River") {
+        return tripsList.filter(trip => trip.tripType === "Past").filter(trip => trip.waterBodyType === "River");
+      }
+      if (filters.waterBodyType === "Lake") {
+        return tripsList.filter(trip => trip.tripType === "Past").filter(trip => trip.waterBodyType === "Lake");
+      }
+      if (filters.waterBodyType === "Ocean") {
+        return tripsList.filter(trip => trip.tripType === "Past").filter(trip => trip.waterBodyType === "Ocean");
+      }
+      if (filters.waterBodyType === "Mix") {
+        return tripsList.filter(trip => trip.tripType === "Past").filter(trip => trip.waterBodyType === "Mix");
+      }
       return tripsList.filter(trip => trip.tripType === "Past");
-    } else if (filter === "Future") {
-      return tripsList.filter(trip => trip.tripType === "Future");
-    } else {
-      return tripsList;
     }
 
-  }, [filter, tripsList]);
+    // filter by future trip type and additional criteria
+    if (filters.tripType === "Future") {
+      if (filters.waterBodyType === "River") {
+        return tripsList.filter(trip => trip.tripType === "Future").filter(trip => trip.waterBodyType === "River");
+      }
+      if (filters.waterBodyType === "Lake") {
+        return tripsList.filter(trip => trip.tripType === "Future").filter(trip => trip.waterBodyType === "Lake");
+      }
+      if (filters.waterBodyType === "Ocean") {
+        return tripsList.filter(trip => trip.tripType === "Future").filter(trip => trip.waterBodyType === "Ocean");
+      }
+      if (filters.waterBodyType === "Mix") {
+        return tripsList.filter(trip => trip.tripType === "Future").filter(trip => trip.waterBodyType === "Mix");
+      }
+      return tripsList.filter(trip => trip.tripType === "Future");
+    }
 
-  const handleFilterSelection = (event) => {
-    setFilter(event.target.value);
+    // filter by water body type for all trips
+    if (filters.waterBodyType === "River") {
+      return tripsList.filter(trip => trip.waterBodyType === "River");
+    }
+
+    if (filters.waterBodyType === "Lake") {
+      return tripsList.filter(trip => trip.waterBodyType === "Lake");
+    }
+
+    if (filters.waterBodyType === "Ocean") {
+      return tripsList.filter(trip => trip.waterBodyType === "Ocean");
+    }
+
+    if (filters.waterBodyType === "Mix") {
+      return tripsList.filter(trip => trip.waterBodyType === "Mix");
+    }
+
+    return tripsList;
+
+  }, [filters, tripsList]);
+
+  const handleTypeSelection = (event) => {
+    setFilters(prev => ({ ...prev, tripType: event.target.value }));
   };
 
+  const handleWaterBodySelection = (event) => {
+    setFilters(prev => ({ ...prev, waterBodyType: event.target.value }));
+  }
+
+  console.log("Filters: ", filters);
   return (
     <React.Fragment>
       <div className="row justify-content-center">
         <div className="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2">
           {tripsList.length > 0 && (
-            <React.Fragment>
+            <div className="filter-options">
               <label style={{ justifyContent: "left" }}>Filter Trips</label>
               <select
-                defaultValue={filter}
+                defaultValue={filters}
                 className="form-select"
-                onChange={handleFilterSelection}>
+                onChange={handleTypeSelection}>
                 <option value="" disabled>Select trip type</option>
                 <option value="All">All</option>
                 <option value="Past">Past</option>
                 <option value="Future">Future</option>
               </select>
+              <select
+                defaultValue={filters}
+                className="form-select"
+                onChange={handleWaterBodySelection}>
+                <option value="" disabled>Select water body type</option>
+                <option value="Any">Any</option>
+                <option value="River">River</option>
+                <option value="Lake">Lake</option>
+                <option value="Ocean">Ocean</option>
+                <option value="Mix">Mix</option>
+              </select>
               <br/>
-            </React.Fragment>
+            </div>
           )}
         </div>
       </div>
